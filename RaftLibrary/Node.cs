d1a7d6d7707ee0d0a32451f9f79533a0edab5353
Dorian C <thenewdorian21@gmail.com>
2025-01-11 19:27:40 -0700

    Add project files.

---
+++ RaftLibrary/Node.cs
@@ -0,0 +1,25 @@
+﻿namespace RaftLibrary
+{
+    public class Node
+    {
+        int _term;
+
+        public Node(int term)
+        {
+            _term = term;
+        }
+
+        public void TimeoutElection()
+        {
+            _term++;
+        }
+
+        public int GetTerm()
+        {
+            return _term;
+        }
+
+
+
+    }
+}
---
+++ RaftTests/FollowerTests.cs
@@ -0,0 +1,15 @@
+﻿using RaftLibrary;
+
+namespace RaftTests
+{
+    public class FollowerTests
+    {
+        [Fact]
+        public void TestFollowerTermIncrementOnElectionTimeout()
+        {
+            Node node = new Node(1);
+            node.TimeoutElection();
+            Assert.Equal(2, node.GetTerm());
+        }
+    }
+}
